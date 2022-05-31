95803473cc56e2d2a3d8c87736cbb986c3d99b49
RoozbehZhuleh <zhuleh.developer@gmail.com>
2022-05-31 13:13:11 +0430

    update

--- docs/app.js
+++ docs/app.js
@@ -11,4 +11,13 @@ navLink.classList.add(".active");
 })
 
 
-document.querySelector(".nav__subscription").style.setProperty('--animate-duration', '4s');
+document.querySelector(".nav__subscription").style.setProperty('--animate-duration', '3s');
+document.querySelector(".homework__1").style.setProperty('--animate-duration', '1.5s');
+document.querySelector(".homework__2").style.setProperty('--animate-duration', '2s');
+document.querySelector(".homework__3").style.setProperty('--animate-duration', '3s');
+document.querySelector(".nav__links__dashboard").style.setProperty('--animate-duration', '1s');
+document.querySelector(".nav__links__courses").style.setProperty('--animate-duration', '1.2s');
+document.querySelector(".nav__links__chats").style.setProperty('--animate-duration', '1.4s');
+document.querySelector(".nav__links__grades").style.setProperty('--animate-duration', '1.6s');
+document.querySelector(".nav__links__schedule").style.setProperty('--animate-duration', '1.8s');
+document.querySelector(".nav__links__settings").style.setProperty('--animate-duration', '2s');
