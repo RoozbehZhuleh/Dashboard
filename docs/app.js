31890fdb76103228c09ac882cf87652991858eca
RoozbehZhuleh <zhuleh.developer@gmail.com>
2022-05-31 13:02:16 +0430

    update

--- docs/app.js
+++ docs/app.js
@@ -3,3 +3,12 @@ flatpickr(".calendar__body", {
      enableTime: true,
     dateFormat: "Y-m-d H:i",
 });
+
+const navLink = document.querySelector(".nav__link");
+
+navLink.addEventListener("click", () => {
+navLink.classList.add(".active");
+})
+
+
+document.querySelector(".nav__subscription").style.setProperty('--animate-duration', '4s');
